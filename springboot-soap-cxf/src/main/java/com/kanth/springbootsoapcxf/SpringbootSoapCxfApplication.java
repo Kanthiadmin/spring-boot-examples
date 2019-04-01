98ac500a5d597ae757e73943a2c1784593cda8cd
ramakanth.b <ramakanth.b@BLR-NB0020>
2019-04-01 14:48:06 +0530

    spring soap cxf intial

---
+++ springboot-soap-cxf/src/main/java/com/kanth/springbootsoapcxf/SpringbootSoapCxfApplication.java
@@ -0,0 +1,13 @@
+package com.kanth.springbootsoapcxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapCxfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSoapCxfApplication.class, args);
+	}
+
+}
